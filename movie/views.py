c9fe8410aa4feaa20bc99364988dba36d574ecb6
Emmanuel Alvarez Castrillon <ealvarezc1@eafit.edu.co>
2025-07-31 13:13:29 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse("<h1>Welcome to home page</h1>")
     #return render(request, 'home.html')
-    return render(request, 'home.html', {"name": "Emmanuel Alvarez Castrillon"})
+    #return render(request, 'home.html', {"name": "Emmanuel Alvarez Castrillon"})
+    searchTerm = request.GET.get("SearchMovie")
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {"name": "Emmanuel Alvarez Castrillon", "searchTerm": searchTerm, "movies": movies})
 def about(request):
     #return HttpResponse("<h1>Welcome to about page</h1>")
     return render(request, 'about.html')
